49cb9993e7631493633d1f0e1c2c89121156ae0c
RachitG54 <rachit0596@gmail.com>
2017-09-17 11:40:17 +0530

    script-update [labels]

--- data/script.py
+++ data/script.py
@@ -1,20 +1,17 @@
 # -*- coding: utf-8 -*-
 import urllib
+import json
 #urllib.urlretrieve("https://firebasestorage.googleapis.com/v0/b/lynkhacksmock.appspot.com/o/10.jpg?alt=media&token=538dec79-1f84-4c47-8185-bfcc3ce4aa3a", "local-filename.jpg")
 
-file = open('dataset.json', 'rb') 
-arr = file.readlines()
-arr2 =  [x.strip() for x in arr[0].split('\"')]
-imgnme = ""
-url = []
-for i in xrange(len(arr2)):
-	if(arr2[i]=='imageuid'):
-		i+=2
-		imgnme = 'lynk/' + arr2[i]+'.jpg'
-	elif(arr2[i]=='url'):
-		i+=2
-		imgurl = arr2[i]
-		url.append(imgurl)
-		print imgnme+"\n"+imgurl
-		urllib.urlretrieve(imgurl,imgnme)
-print len(url)
+file = open('dataset.json', 'r') 
+labelmap = {}
+
+data = json.load(file)
+
+for elem in data:
+	img_name = 'lynk/'+elem['imageuid'] + '.jpg'
+	urllib.urlretrieve(elem['url'], img_name)
+	labelmap[elem['imageuid']] = elem['name']
+
+with open('labels.json','w') as f :
+	f.write(json.dumps(labelmap))
